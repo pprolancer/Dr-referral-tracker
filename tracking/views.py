a268b7437a5b89e8817853429c134718b4c49aa9
Vincent <vincent@vincentdavis.net>
2015-04-19 18:07:51 -0600

    Add mockup of basic report view

--- tracking/views.py
+++ tracking/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+class ReferralReport(ListView):
+    """
+    See Referral Report.pdf in misc folder for example of this view
+    """
