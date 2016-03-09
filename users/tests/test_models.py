b1ed9a20ccd6da91002a0b5d253a8acd72994e83
pprolancer <pprolancer@gmail.com>
2016-03-09 17:44:55 -0500

    issue#36: Tests for issue 34 added some testcases to show: 1- when a user registered that is in_active 2- when we change a user model the user will not change to in_active=True

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,1 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class UserAuthTest(TestCase):
+    ''' a testcases class for Physician model '''
+
+    def setUp(self):
+        ''' setup initial objects '''
+        self.default_pass = 'pass1234'
+        self.user = User.objects.create_user(username='user1',
+                                             password=self.default_pass)
+
+    def test_change_user_does_not_make_inactive(self):
+        ''' quantifiedcode: ignore it! '''
+
+        self.assertTrue(self.user.is_active)
+        self.user.first_name = 'firstname1'
+        self.user.save()
+        user = User.objects.get(username=self.user.username)
+        self.assertTrue(user.is_active)
+        self.user.email = 'sample@email.com'
+        self.user.save()
+        user = User.objects.get(username=self.user.username)
+        self.assertTrue(user.is_active)
--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,1 +1,48 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class RegisterViewTest(TestCase):
+    ''' testcases class for RegisterView '''
+
+    def setUp(self):
+        '''initial default user to be login in '''
+        pass
+
+    def test_register(self):
+        ''' quantifiedcode: ignore it! '''
+        default_pass = 'pass1234'
+        data = dict(
+            username='user1', password=default_pass, password1=default_pass
+        )
+        response = self.client.post(reverse('user_register'), data)
+        self.assertEqual(response.status_code, 302)
+        query = User.objects.filter(username=data['username'])
+        self.assertEqual(query.count(), 1)
+        user = query[0]
+        self.assertTrue(user.check_password(default_pass))
+        self.assertFalse(user.is_active)
+
+    def test_register_missing_data(self):
+        ''' quantifiedcode: ignore it! '''
+        data = dict(username='user1')
+        response = self.client.post(reverse('user_register'), data)
+        self.assertEqual(response.status_code, 200)
+        query = User.objects.filter(username=data['username'])
+        self.assertEqual(query.count(), 0)
+        context = response.context
+        form = context['form']
+        self.assertIn('password',  form.errors)
+        self.assertIn('password1',  form.errors)
+
+    def test_register_no_match_password(self):
+        ''' quantifiedcode: ignore it! '''
+        data = dict(username='user1', password='pass1234', password1='nomatch')
+        response = self.client.post(reverse('user_register'), data)
+        self.assertEqual(response.status_code, 200)
+        query = User.objects.filter(username=data['username'])
+        self.assertEqual(query.count(), 0)
+        context = response.context
+        form = context['form']
+        self.assertIn('password1',  form.errors)
